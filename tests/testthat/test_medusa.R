701b3461e7f44444bbc1c274deecb8dbb6e37a3d
klau506 <claudia.rodes@bc3research.org>
2024-02-21 15:15:42 +0100

    Try fix tests v2: imager pkg

--- tests/testthat/test_medusa.R
+++ tests/testthat/test_medusa.R
@@ -206,8 +206,8 @@ test_that("Test10_Impact & basic graph", {
   #Check figures
   vars <- c("AGERP", "COUNTRYRP", "GENDERRP", "HHTYPE", "MUNISIZE", "REGION", "REGMR", "STUDIESRP", "ZONE" )
   for (g in vars) {
-    test_result <- png::readPNG(file.path(rprojroot::find_root(rprojroot::is_testthat), paste0("test_inputs/figures/DI_",g,".png")))
-    test_expect <- png::readPNG(file.path(rprojroot::find_root(rprojroot::is_testthat), paste0("test_outputs/figures/DI_",g,".png")))
+    test_result <- imager::load.image(file.path(rprojroot::find_root(rprojroot::is_testthat), paste0("test_inputs/figures/DI_",g,".png")))
+    test_expect <- imager::load.image(file.path(rprojroot::find_root(rprojroot::is_testthat), paste0("test_outputs/figures/DI_",g,".png")))
 
     testthat::expect_equal(test_result, test_expect)
   }
